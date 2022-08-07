af0fdd661d083e15a93f4c2a6929a3808db19265
nikhilfeb95 <nikhil.mollay@gmail.com>
2022-08-07 17:37:51 -0400

    updated the code

--- LoadXML2DB.Mollay.Shah.R
+++ LoadXML2DB.Mollay.Shah.R
@@ -40,14 +40,12 @@ dbExecute(dbcon, "CREATE TABLE IF NOT EXISTS CITED_MEDIUM(
 
 dbExecute(dbcon, "CREATE TABLE IF NOT EXISTS JOURNAL (
           issn VARCHAR(255) PRIMARY KEY,
-          issn_type_id INT,
-          cited_medium_id INT,
+          issn_type VARCHAR(255),
+          cited_medium VARCHAR(255),
           volume INT,
           pubDate DATE,
           title VARCHAR(255),
-          iso_abbreviation VARCHAR(255),
-          FOREIGN KEY issn_type_constraint(issn_type_id) REFERENCES issn_type(id),
-          FOREIGN KEY cited_medium_constraint (cited_medium_id) REFERENCES cited_medium(id)
+          isoabbreviation VARCHAR(255),
 )")
 
 dbExecute(dbcon, "CREATE TABLE IF NOT EXISTS ARTICLE (
@@ -60,9 +58,86 @@ dbExecute(dbcon, "CREATE TABLE IF NOT EXISTS ARTICLE (
           FOREIGN KEY author_constraint(author_id) REFERENCES author(author_id)
           )")
 
-
-parseDate <- function(day, year, month) {
+parseMonth <- function(year){
+  numbericMonth <- "0"
   
+  switch(month, 
+         Jan={
+           numbericMonth <- "01"
+         },
+         Feb={
+           numbericMonth <- "02"
+         },
+         Mar={
+           numbericMonth <- "03"
+         },
+         Apr={
+           numbericMonth <- "04"
+         },
+         May={
+           numbericMonth <- "05"
+         },
+         Jun={
+           numbericMonth <- "06"
+         },
+         Jul={
+           numbericMonth <- "07"
+         },
+         Aug={
+           numbericMonth <- "08"
+         },
+         Sep={
+           numbericMonth <- "09"
+         },
+         Oct={
+           numbericMonth <- "10"
+         },
+         Nov={
+           numbericMonth <- "11"
+         },
+         Dec={
+           numbericMonth <- "12"
+         }
+  )
+  return (numbericMonth)
+}
+
+#Assuming that if the day data is not present it is the 1st
+parseDate <- function(pubDate) {
+  day <- 0
+  month <- 0
+  year <- 0
+  if(is.null(pubDate[['Day']])){
+    #assume day to be first
+    day <- 1
+  }
+  else{
+    day <- pubDate[['Day']]
+  }
+  
+  if(is.null(pubDate[['Month']])){
+    #assume day to be first
+    month <- 1
+  }
+  else{
+    month <- parseMonth(pubDate[['Month']])
+  }
+  if(is.null(pubDate[['Year']])){
+    #assume day to be first
+    year <- 1901
+  }
+  else{
+    month <- pubDate[['Year']]
+  }
+  formattedDate <- paste0(year,"-",month,"-",date)
+  
+  return(formattedDate)
+}
+
+parse_attrs <- function(attrs){
+  for(char in attrs){
+    return(char)
+  }
 }
 
 
@@ -87,9 +162,25 @@ for(i in 1:numberOfPubs){
   
   #parse the journal from here
   journal <- article[[1]]
-  
+  issn <- journal[['ISSN']]
   x <- xmlAttrs(journal[[1]])
-  print(typeof(x))
+  issn_type <- parse_attrs(x)
+  x <- xmlAttrs(journal[[2]])
+  cited_medium <- parse_attrs(x)
+  journal_issue <- journal[['JournalIssue']]
+  volume <- strtoi(journal_issue[['Volume']])
+  issue <- journal_issue[['Issue']]
+  
+  pubDate <- parseDate(journal_issue[['PubDate']])
+  journal_title <- journal[["Title"]]
+  iso_abbr < - journal[["ISOAbbreviation"]]
+  format <- "%Y-%m-%d"
+  
+  #Insert into the journals table
+  query <- sprintf("INSERT INTO journals (issn, issn_type,cited_medium,volume, pubDate, title, isoabbreviation) values('%s','%s',%s, %d,STR_TO_DATE('%s','%s'), %s, %s) On duplicate key update pubDate=STR_TO_DATE('%s','%s') "
+                   ,issn,issn_type,cited_medium,volume,pubDate,format,journal_title,iso_abbr,pubDate,format)
+  
+  #fetch info for the languages and the authors
 }
 
 
